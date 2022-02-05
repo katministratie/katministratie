2da2482c3f5738912f909978aa2a7ef6037fd93d
Johan de Kroon <johandekroon@gmail.com>
2022-02-05 13:58:46 +0100

    Layout and colors for the kat info

--- Superkatten.Katministratie.Application/Contracts/Superkat.cs
+++ Superkatten.Katministratie.Application/Contracts/Superkat.cs
@@ -4,8 +4,9 @@ namespace Superkatten.Katministratie.Application.Contracts
 {
     public class Superkat
     {
+        public string Picture { get; set; } = string.Empty;
         public int Number { get; init; }
-        public string Name { get; init; }
+        public string Name { get; init; } = string.Empty;
         public DateTimeOffset FoundDate { get; init; }
     }
 }
