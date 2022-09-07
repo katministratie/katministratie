0a3726768dc2d4f0d7cc22687b3b8e01740b4e06
Johan de Kroon <johandekroon@gmail.com>
2022-09-07 08:41:09 +0200

    Add footer to kooikaart Add not logged in message default to dev environment

--- Superkatten.Katministratie.Application/CageCard/Details/CageCardDefaultFooterComposer.cs
+++ Superkatten.Katministratie.Application/CageCard/Details/CageCardDefaultFooterComposer.cs
@@ -1,6 +1,8 @@
 ﻿using QuestPDF.Drawing;
 using QuestPDF.Fluent;
 using QuestPDF.Infrastructure;
+using SkiaSharp;
+using Superkatten.Katministratie.Application.CageCard.Details.SuperkatCard;
 using Superkatten.Katministratie.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -19,11 +21,38 @@ namespace Superkatten.Katministratie.Application.CageCard.Details
 
         public void Compose(IContainer container)
         {
+            var foods = _superkatten
+                .Select(s => s.FoodType)
+                .Distinct()
+                .ToList();
+
+            var litterTypes = _superkatten
+                .Select(s => s.LitterType)
+                .Distinct()
+                .ToList();
+
+            var wedFoods = _superkatten
+                .Select(s => s.WetFoodAllowed)
+                .Distinct()
+                .ToList();
+
             container
                 .PaddingTop(10)
                 .Column(column =>
                 {
+                    column.Item()
+                        .Text(string.Join("-", foods));
                     column.Spacing(5);
+
+                    column.Item()
+                        .Text(string.Join("-", litterTypes));
+
+                    column.Spacing(5);
+
+                    column.Item()
+                        .Text(string.Join("-", wedFoods));
+                    column.Spacing(5);
+
                     column.Item()
                         .BorderBottom(1)
                         .PaddingBottom(5)
