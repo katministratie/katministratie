f6b4a76d3738918f950c6d8f66804e4379903e1c
r-go-tools <jokroon@rademaker.nl>
2022-09-12 16:53:29 +0200

    remove blank line

--- Superkatten.Katministratie.Application/Mappers/ISuperkatMapper.cs
+++ Superkatten.Katministratie.Application/Mappers/ISuperkatMapper.cs
@@ -7,10 +7,7 @@ namespace Superkatten.Katministratie.Application.Mappers
     public interface ISuperkatMapper
     {
         Superkat MapContractToDomain(ContractEntitiess.Superkat superkat);
-
         ContractEntitiess.Superkat MapDomainToContract(Superkat createdSuperkat);
-
-
         CatArea MapContractToDomain(ContractEntitiess.CatArea area);
         CatBehaviour MapContractToDomain(ContractEntitiess.CatBehaviour behaviour);
         Gender MapContractToDomain(ContractEntitiess.Gender gender);
